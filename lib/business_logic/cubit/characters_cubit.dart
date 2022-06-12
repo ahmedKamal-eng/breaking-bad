6da6a7a4fa51944897bd48577c9ace32f97eeef5
ahmedKaml-eng <ahmed.kamal22156@gmail.com>
2022-06-12 23:31:48 +0200

    first

---
+++ lib/business_logic/cubit/characters_cubit.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:bloc/bloc.dart';
+import 'package:breaking_bad_app/business_logic/cubit/characters_state.dart';
+
+class CharactersCubit extends Cubit<CharactersState>{
+  CharactersCubit(super.initialState);
+
+
+}
